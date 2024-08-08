186bca80863bd0c154b13e63501558d2760360f8
Ken <keniis@126.com>
2024-08-08 19:19:57 +0800

    fix

--- src/bin/page_sort.rs
+++ src/bin/page_sort.rs
@@ -1,4 +1,4 @@
-use std::{fs::{self, rename, File}, io::{Error, Read, Seek}, path::PathBuf};
+use std::{fs::{self, File}, io::{Error, Read}, os::windows::fs::FileExt, path::PathBuf};
 
 use clap::Parser;
 use indicatif::ProgressStyle;
@@ -13,6 +13,15 @@ struct Arguments {
     color: bool,
 
     #[arg(
+        short = 'o',
+        long = "output",
+        default_value = "output",
+        help = "output file name",
+        required = true
+    )]
+    output: PathBuf,
+
+    #[arg(
         help = "Page(s) file directory, should contain one or multiple raw 16K page",
         value_name = "PAGE_FILE_DIR"
     )]
@@ -23,25 +32,11 @@ fn arr2int(buf:&[u8; 4]) -> u32{
     ((buf[0] as u32) << 24) | ((buf[1] as u32) << 16) | ((buf[2] as u32) << 8) | (buf[3] as u32)
 }
 
-fn get_page_number(path: &PathBuf) -> Result<u32, Error>{
-    File::open(path)
-        .and_then(|mut file| {
-            match file.seek(std::io::SeekFrom::Start(0x4)){
-                Ok(_) => Ok(file),
-                Err(err) => {
-                    Err(err)
-                }
-            }
-        })
-        .and_then(|mut file| {
-            let mut buffer = [0; 4];
-            match file.read_exact(&mut buffer){
-                Ok(_) => Ok(arr2int(&buffer)),
-                Err(err) => {
-                    Err(err)
-                }
-            }
-        })
+fn get_page_number(path: &PathBuf) -> Result<u64, Error>{
+    let page = File::open(path)?;
+    let mut buffer = [0; 4];
+    page.seek_read(&mut buffer, 4)?;
+    Ok(arr2int(&buffer) as u64)
 }
 
 fn get_file_count(path: PathBuf) -> Result<usize, Error>{
@@ -51,7 +46,23 @@ fn get_file_count(path: PathBuf) -> Result<usize, Error>{
         })
 }
 
+fn copy_page(source: &PathBuf, destination: &File, offset: u64) -> Result<(), Error>{
+    let mut source_page = File::open(source)?;
+    let mut buffer = [0; 4096];
+    let mut offset = offset;
+    loop {
+        let bytes_read = source_page.read(&mut buffer)?;
+        if bytes_read == 0 {
+            break;
+        }
+        destination.seek_write(&buffer, offset)?;
+        offset += bytes_read as u64;
+    }
+    Ok(())
+}
+
 fn main(){
+    const PAGE_SIZE: usize = 16384;
     let args = Arguments::parse();
 
     let subscriber = tracing_subscriber::FmtSubscriber::builder()
@@ -64,6 +75,8 @@ fn main(){
         .without_time()
         .finish();
     tracing::subscriber::set_global_default(subscriber).expect("Failed to setup Logger");
+
+    let output_file = File::create_new(args.output).expect("Failed to open output file");
 
     let entries = fs::read_dir(args.directory.clone()).expect("Failed to read directory");
     let total = get_file_count(args.directory).expect("Failed to get file count");
@@ -85,7 +98,6 @@ fn main(){
     }
 
     for entry in entries {
-
         let path = match entry {
             Ok(entry) => entry.path(),
             Err(entry) => {
@@ -102,23 +114,22 @@ fn main(){
             Some(_) => {}
         };
 
-        let page_number = match get_page_number(&path){
-            Ok(page_number) => page_number,
+        let offset = match get_page_number(&path){
+            Ok(page_number) => page_number * PAGE_SIZE as u64,
             Err(err) => {
                 warn!("Failed to get page number: {}", err);
                 continue;
             }
         };
-        
-        let new_name = format!("{}/sorted_{:0>10}.page", path.parent().unwrap().to_str().unwrap(), page_number);
 
-        match rename(path, new_name){
+        match copy_page(&path, &output_file, offset){
             Ok(_) => {},
             Err(err) => {
-                warn!("Failed to rename: {}", err);
+                warn!("Failed to copy page: {}", err);
                 continue;
             }
         }
+        
         success += 1;
         if let Some(process_bar) = &process_bar{
             process_bar.inc(1);
