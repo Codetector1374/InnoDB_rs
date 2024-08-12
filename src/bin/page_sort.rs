6a56da0cb8314643b37e3a50a94eb44a469929bd
Ken <keniis@126.com>
2024-08-12 18:05:46 +0800

    fix operation system requirement

--- src/bin/page_sort.rs
+++ src/bin/page_sort.rs
@@ -1,4 +1,4 @@
-use std::{fs::File, io::{Error, Seek}, os::windows::fs::FileExt, path::PathBuf};
+use std::{fs::File, io::{Error, Read, Seek, Write}, path::PathBuf};
 
 use clap::Parser;
 use indicatif::{ProgressBar, ProgressStyle};
@@ -32,22 +32,27 @@ fn arr2int(buf:&[u8; 4]) -> u32{
     ((buf[0] as u32) << 24) | ((buf[1] as u32) << 16) | ((buf[2] as u32) << 8) | (buf[3] as u32)
 }
 
-fn get_page_number(pages: &File, offset: u64) -> Result<u64, Error>{
+fn get_page_number(pages: &mut File, offset: u64) -> Result<u64, Error>{
     let mut buffer = [0; 4];
-    pages.seek_read(&mut buffer, offset + 4)?;
+    // pages.seek_read(&mut buffer, offset + 4)?;
+    pages.seek(std::io::SeekFrom::Start(offset+4))?;
+    pages.read(&mut buffer)?;
     Ok(arr2int(&buffer) as u64)
 }
 
-fn copy_page(source: &File, destination: &File, source_offset: u64, destination_offset: u64) -> Result<(), Error>{
+fn copy_page(source: &mut File, destination: &mut File, source_offset: u64, destination_offset: u64) -> Result<(), Error>{
     let mut buffer = [0; 4096];
     let mut destination_offset = destination_offset;
     let mut source_offset = source_offset;
     loop {
-        let bytes_read = source.seek_read(&mut buffer, source_offset)?;
+        // let bytes_read = source.seek_read(&mut buffer, source_offset)?;
+        source.seek(std::io::SeekFrom::Start(source_offset))?;
+        let bytes_read = source.read(&mut buffer)?;
         if bytes_read == 0 {
             break;
         }
-        destination.seek_write(&buffer, destination_offset)?;
+        destination.seek(std::io::SeekFrom::Start(destination_offset))?;
+        destination.write(&buffer)?;
         destination_offset += bytes_read as u64;
         source_offset += bytes_read as u64;
     }
@@ -69,7 +74,7 @@ fn main(){
         .finish();
     tracing::subscriber::set_global_default(subscriber).expect("Failed to setup Logger");
 
-    let output_file = File::create_new(args.output).expect("Failed to open output file");
+    let mut output_file = File::create_new(args.output).expect("Failed to open output file");
     let mut input_file = File::open(args.input).expect("Failed to open input file");
 
     let total_bytes = input_file.seek(std::io::SeekFrom::End(0)).expect("Failed to get input file size");
@@ -94,7 +99,7 @@ fn main(){
 
     for i in 0..total_pages{
         let offset = i * PAGE_SIZE as u64;
-        let page_number = match get_page_number(&input_file, offset){
+        let page_number = match get_page_number(&mut input_file, offset){
             Ok(page_number) => page_number,
             Err(err) => {
                 warn!("Failed to get page number of page {}: {}.Skip.", i + 1, err);
@@ -102,7 +107,7 @@ fn main(){
             }
         };
         let destination_offset = page_number * PAGE_SIZE as u64;
-        match copy_page(&input_file, &output_file, offset, destination_offset){
+        match copy_page(&mut input_file, &mut output_file, offset, destination_offset){
             Ok(_) => {},
             Err(err) => {
                 warn!("Failed to copy page {}: {}.Skip.", i + 1, err);
